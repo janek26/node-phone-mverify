cc60f18a147fa01d1fb6f130a4e4bac3b5c94981
janek26 <janek.rahrt@me.com>
2015-11-26 13:00:11 +0100

    First Try

---
+++ app.js
@@ -0,0 +1,38 @@
+var unirest = require('unirest');
+var hookUrl = "https://webaroo-mobile-verification.p.mashape.com/mobileVerification";
+
+module.exports = function(apikey) {
+  if (!isValidAPIKey(apikey)) {
+    throw new Error("No valid API Key!");
+  }
+  return function(phone, cb){
+    var url = hookUrl +"?"+
+    "phone="+phone
+    unirest.get(url)
+    .header("X-Mashape-Key", apikey)
+    .header("Accept", "application/json")
+    .end(function (response) {
+      if (response.status == 200){
+        cb({
+          getAPIKey: function() {
+            return apikey;
+          },
+          getNumber: function() {
+            return phone;
+          },
+          success: function() {
+            return response.body.status;
+          },
+          response: function() {
+            return response.body.data;
+          }
+        });
+      }
+    });
+  }
+}
+
+function isValidAPIKey(apikey) {
+  var regex = /^[a-fA-F0-9]{50}$/g;
+  return regex.test(apikey.substring(3));
+}
